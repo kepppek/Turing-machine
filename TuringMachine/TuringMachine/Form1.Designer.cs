1f25d39623e7e815952bd5f5e3537551792729a1
kepppek <113108516+kepppek@users.noreply.github.com>
2023-06-19 18:07:02 +0300

    main

---
+++ TuringMachine/TuringMachine/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace TuringMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle28 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle29 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle30 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lenta = new System.Windows.Forms.DataGridView();
+            this.right = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.Button();
+            this.numbers = new System.Windows.Forms.DataGridView();
+            this.command = new System.Windows.Forms.DataGridView();
+            this.alfavit = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.restart = new System.Windows.Forms.Button();
+            this.play = new System.Windows.Forms.Button();
+            this.cb2 = new System.Windows.Forms.ComboBox();
+            this.cb3 = new System.Windows.Forms.ComboBox();
+            this.minus = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.lenta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numbers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.command)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alfavit)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lenta
+            // 
+            this.lenta.AllowUserToAddRows = false;
+            this.lenta.AllowUserToDeleteRows = false;
+            this.lenta.AllowUserToResizeColumns = false;
+            this.lenta.AllowUserToResizeRows = false;
+            this.lenta.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.lenta.ColumnHeadersHeight = 25;
+            this.lenta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.lenta.ColumnHeadersVisible = false;
+            dataGridViewCellStyle28.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle28.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle28.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle28.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle28.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle28.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle28.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.lenta.DefaultCellStyle = dataGridViewCellStyle28;
+            this.lenta.Location = new System.Drawing.Point(30, 37);
+            this.lenta.Name = "lenta";
+            this.lenta.ReadOnly = true;
+            this.lenta.RowHeadersVisible = false;
+            this.lenta.RowHeadersWidth = 25;
+            this.lenta.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.lenta.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.lenta.Size = new System.Drawing.Size(570, 25);
+            this.lenta.TabIndex = 23;
+            this.lenta.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // right
+            // 
+            this.right.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.right.Location = new System.Drawing.Point(606, 37);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(19, 25);
+            this.right.TabIndex = 25;
+            this.right.Text = ">";
+            this.right.UseVisualStyleBackColor = true;
+            this.right.Click += new System.EventHandler(this.right_Click);
+            // 
+            // left
+            // 
+            this.left.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.left.Location = new System.Drawing.Point(5, 37);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(19, 25);
+            this.left.TabIndex = 28;
+            this.left.Text = "<";
+            this.left.UseVisualStyleBackColor = true;
+            this.left.Click += new System.EventHandler(this.left_Click);
+            // 
+            // numbers
+            // 
+            this.numbers.AllowUserToAddRows = false;
+            this.numbers.AllowUserToDeleteRows = false;
+            this.numbers.AllowUserToResizeColumns = false;
+            this.numbers.AllowUserToResizeRows = false;
+            this.numbers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.numbers.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.numbers.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle29.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle29.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle29.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle29.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle29.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle29.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle29.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.numbers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle29;
+            this.numbers.ColumnHeadersHeight = 25;
+            this.numbers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.numbers.ColumnHeadersVisible = false;
+            dataGridViewCellStyle30.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle30.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle30.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle30.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle30.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle30.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle30.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.numbers.DefaultCellStyle = dataGridViewCellStyle30;
+            this.numbers.GridColor = System.Drawing.SystemColors.Control;
+            this.numbers.Location = new System.Drawing.Point(30, 13);
+            this.numbers.Name = "numbers";
+            this.numbers.ReadOnly = true;
+            this.numbers.RowHeadersVisible = false;
+            this.numbers.RowHeadersWidth = 25;
+            this.numbers.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.numbers.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.numbers.Size = new System.Drawing.Size(570, 25);
+            this.numbers.TabIndex = 30;
+            // 
+            // command
+            // 
+            this.command.AllowUserToAddRows = false;
+            this.command.AllowUserToResizeColumns = false;
+            this.command.AllowUserToResizeRows = false;
+            this.command.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.command.Location = new System.Drawing.Point(5, 127);
+            this.command.Name = "command";
+            this.command.ReadOnly = true;
+            this.command.Size = new System.Drawing.Size(403, 319);
+            this.command.TabIndex = 33;
+            this.command.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.command_CellMouseClick);
+            // 
+            // alfavit
+            // 
+            this.alfavit.AllowUserToResizeColumns = false;
+            this.alfavit.AllowUserToResizeRows = false;
+            this.alfavit.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.alfavit.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.alfavit.Location = new System.Drawing.Point(414, 127);
+            this.alfavit.Name = "alfavit";
+            this.alfavit.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.alfavit.Size = new System.Drawing.Size(201, 319);
+            this.alfavit.TabIndex = 34;
+            this.alfavit.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.alfavit_CellClick);
+            this.alfavit.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.alfavit_CellEndEdit);
+            this.alfavit.UserDeletedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.alfavit_UserDeletedRow);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cb3);
+            this.groupBox1.Controls.Add(this.cb2);
+            this.groupBox1.Controls.Add(this.cb1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(277, 53);
+            this.groupBox1.TabIndex = 42;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Конструктор команд";
+            // 
+            // cb1
+            // 
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Location = new System.Drawing.Point(14, 19);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(79, 21);
+            this.cb1.TabIndex = 42;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.restart);
+            this.groupBox2.Controls.Add(this.play);
+            this.groupBox2.Location = new System.Drawing.Point(414, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 53);
+            this.groupBox2.TabIndex = 43;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Кнопки управления";
+            // 
+            // restart
+            // 
+            this.restart.Location = new System.Drawing.Point(34, 19);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(22, 23);
+            this.restart.TabIndex = 42;
+            this.restart.Text = "■";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // play
+            // 
+            this.play.Location = new System.Drawing.Point(6, 19);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(22, 23);
+            this.play.TabIndex = 41;
+            this.play.Text = "▶";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // cb2
+            // 
+            this.cb2.FormattingEnabled = true;
+            this.cb2.Location = new System.Drawing.Point(99, 19);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(79, 21);
+            this.cb2.TabIndex = 43;
+            // 
+            // cb3
+            // 
+            this.cb3.FormattingEnabled = true;
+            this.cb3.Location = new System.Drawing.Point(184, 18);
+            this.cb3.Name = "cb3";
+            this.cb3.Size = new System.Drawing.Size(79, 21);
+            this.cb3.TabIndex = 44;
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(43, 19);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(25, 23);
+            this.minus.TabIndex = 48;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(12, 19);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(25, 23);
+            this.plus.TabIndex = 47;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.minus);
+            this.groupBox3.Controls.Add(this.plus);
+            this.groupBox3.Location = new System.Drawing.Point(288, 68);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(120, 53);
+            this.groupBox3.TabIndex = 49;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Состояния";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.alfavit);
+            this.Controls.Add(this.command);
+            this.Controls.Add(this.numbers);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.lenta);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Машина Тьюринга";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.lenta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numbers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.command)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alfavit)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView lenta;
+        private System.Windows.Forms.Button right;
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.DataGridView numbers;
+        private System.Windows.Forms.DataGridView command;
+        private System.Windows.Forms.DataGridView alfavit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.ComboBox cb3;
+        private System.Windows.Forms.ComboBox cb2;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ TuringMachine/TuringMachine/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TuringMachine
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+        }
+
+        public static int index=0;
+
+        public static void MoveLenta(DataGridView dgv, DataGridView lenta)
+        {
+            for(int i=0;i<27;i++)
+            {
+                dgv.Rows[0].Cells[i].Value = index - 13 + i;
+
+                lenta.Rows[0].Cells[i].Value = data[Index(index - 13 + i)];
+            }
+        }
+
+        public static int Index(int v)
+        {
+            int id = v;
+
+            if (v < 0)
+                id = v * -1 + 999;
+
+            return id;
+        }
+
+        public static void AddValue(int id, string word)
+        {
+            data[Index(id)] = word;
+        }
+
+        public static void DeleteValue(int id)
+        {
+            data[Index(id)] = null;
+        }
+
+        public static string [] data = new string[1999];
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lenta.RowCount = 1;
+            lenta.ColumnCount = 27;
+
+            numbers.RowCount = 1;
+            numbers.ColumnCount = 27;
+           
+            alfavit.Columns.Add("cell","Алфавит");
+
+            lenta.ClearSelection();
+            numbers.ClearSelection();
+
+            numbers.Rows[0].Cells[13].Style.BackColor = Color.Coral;
+            lenta.Rows[0].Cells[13].Style.BackColor = Color.Coral;
+
+            command.Columns.Add("q1","q1");
+            command.Columns.Add("q2", "q2");
+            command.Rows.Add(1);
+            command.Rows[0].HeaderCell.Value = "_";
+
+            cb1.Items.Add("_");
+
+            cb2.Items.Add(">");
+            cb2.Items.Add("<");
+            cb2.Items.Add(".");
+
+            cb3.Items.Add("0");
+            cb3.Items.Add("q1");
+            cb3.Items.Add("q2");
+
+            MoveLenta(numbers,lenta);
+
+            command.ClearSelection();
+        }
+            
+        List<string> a = new List<string>();
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.ColumnIndex < lenta.ColumnCount && e.RowIndex < lenta.RowCount && e.Button == MouseButtons.Right)
+            {
+                lenta.CurrentCell = lenta.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                lenta.CurrentCell.Value = "";
+                DeleteValue(int.Parse(numbers.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()));
+            }
+            else
+            {
+                if (word != "")
+                {
+                    lenta.ClearSelection();
+                    lenta.CurrentCell.Value = word;
+                    AddValue(int.Parse(numbers.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()), word);
+                   
+                }
+            }
+            lenta.ClearSelection();
+        }
+
+        private void right_Click(object sender, EventArgs e)
+        {
+            if (index+1 < 986)
+            {
+                index++;
+                MoveLenta(numbers, lenta);
+            }            
+        }
+
+        private void left_Click(object sender, EventArgs e)
+        {
+            if (index-1 > -986)
+            {
+                index--;
+                MoveLenta(numbers, lenta);
+            }
+        }
+
+        string word = "";
+
+        private void alfavit_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+          if(e.RowIndex>-1)
+          if(alfavit.Rows[e.RowIndex].Cells[0].Value!=null)
+                 word = alfavit.Rows[e.RowIndex].Cells[0].Value.ToString();
+        }
+
+        private void alfavit_UserDeletedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            alfavit.ClearSelection();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] != null)
+                    if (data[i] == word)
+                    {
+                        data[i] = null;
+                        MoveLenta(numbers,lenta);
+                    }
+            }
+           for (int i = 0; i < command.RowCount; i++)
+            {
+              if (command.Rows[i].HeaderCell.Value.ToString() == word)
+                {
+                    cb1.Items.RemoveAt(i-1);
+                    command.Rows.RemoveAt(i);
+                    word = "";
+                    return;
+                }
+            }        
+            
+        }
+
+        private void alfavit_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            alfavit.ClearSelection();
+            for(int i=0;i<alfavit.RowCount-1;i++)
+            {
+                if(alfavit.Rows[i].Cells[0].Value.ToString() == alfavit.Rows[e.RowIndex].Cells[0].Value.ToString() && i!=e.RowIndex)
+                {
+                    alfavit.Rows.RemoveAt(e.RowIndex);
+                    return;
+                }
+            }
+
+            for (int i = 0; i < command.RowCount; i++)
+            {
+                if (command.Rows[i].HeaderCell.Value.ToString() == alfavit.Rows[e.RowIndex].Cells[0].Value.ToString())
+                {
+                    return;
+                }
+            }
+
+            cb1.Items.Add(alfavit.Rows[e.RowIndex].Cells[0].Value.ToString());
+
+            command.Rows.Add(1);
+            command.Rows[command.RowCount-1].HeaderCell.Value = alfavit.Rows[e.RowIndex].Cells[0].Value.ToString();
+        }
+
+        public static int qCount=2;
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            qCount++;
+            command.Columns.Add("q"+qCount, "q"+qCount);
+            cb3.Items.Add("q" + qCount);
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            if (qCount > 2)
+            {
+                cb3.Items.RemoveAt(qCount);
+                command.Columns.RemoveAt(command.ColumnCount - 1);
+                qCount--;
+            }
+        }
+
+        private void command_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                string s = "";
+
+                if (cb1.Text == "" || cb2.Text == "" || cb3.Text == "")
+                {
+                    MessageBox.Show("Поля пусты.", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                s = cb1.Text + cb2.Text + cb3.Text;
+                command.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = s;
+            }
+            else
+                command.ClearSelection();
+        }
+
+        bool stop;
+
+        private async void play_Click(object sender, EventArgs e)
+        {
+            stop = false;
+            int q = 1;
+            string s = "";
+
+            while (true)
+            {
+                command.ClearSelection();
+
+                if(stop)
+                {
+                    return;
+                }
+
+                for(int i=0;i<command.RowCount;i++)
+                {                  
+                    
+                    if(data[Index(index)] == command.Rows[i].HeaderCell.Value.ToString()
+                        || (data[Index(index)]==null && command.Rows[i].HeaderCell.Value.ToString()=="_"))
+                    {
+                        command.Rows[i].Cells[q - 1].Selected = true;
+                        s = command.Rows[i].Cells[q - 1].Value.ToString();
+
+                        data[Index(index)] = s[0].ToString();
+
+                        if (s[0] == '_')
+                            data[Index(index)] = null;
+
+                        switch (s[1])
+                        {
+                            case '>':                            
+                                    index++;
+                                if (index > 986)
+                                {
+                                    index = -986;
+                                }
+                                    MoveLenta(numbers,lenta);
+                                break;
+
+                            case '<':                               
+                                  index--;
+                                if (index < -986)
+                                {
+                                    index = 986;
+                                }
+                                  MoveLenta(numbers, lenta);
+                                break;
+                        }
+
+                        if (s[s.Length - 1] == '0')
+                        {
+                            MessageBox.Show("Выполнение программы завершено", "Информация!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            return;
+                        }
+                        else
+                            q = int.Parse(s[s.Length - 1].ToString());
+                    }
+                    await Task.Delay(100);
+                }
+            }
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+    }
+}
